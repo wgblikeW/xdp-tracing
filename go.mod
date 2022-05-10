9eaad3bcf710520d5980d632fd90274c51f24a1c
p1nant0m <wgblike@outlook.com>
2022-05-10 23:34:29 +0800

    update net: demo of packets capture

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/spf13/cobra v1.4.0
 
 require (
+	github.com/google/gopacket v1.1.19 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
---
+++ net/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"syscall"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+// Convert a uint16 to host byte order (big endian)
+func Htons(v uint16) int {
+	return int((v << 8) | (v >> 8))
+}
+
+type tcpFlags struct {
+	FIN bool
+	SYN bool
+	RST bool
+	PSH bool
+	ACK bool
+	URG bool
+	ECE bool
+	CWR bool
+	NS  bool
+}
+
+const (
+	SPACE = " "
+)
+
+func parseFlagsToString(flags interface{}) string {
+	ret := ""
+	v := reflect.ValueOf(flags).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).Interface().(bool) {
+			ret += v.Type().Field(i).Name + SPACE
+		}
+	}
+	return ret
+}
+
+func main() {
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, Htons(syscall.ETH_P_ALL))
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return
+	}
+	fmt.Println("Listening on Raw Socket")
+	defer syscall.Close(fd)
+
+	for {
+		buf := make([]byte, 65536)
+		_, _, err := syscall.Recvfrom(fd, buf, 0)
+		if err != nil {
+			panic(err)
+		}
+
+		packet := gopacket.NewPacket(buf, layers.LayerTypeEthernet, gopacket.Default)
+		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil && (tcpLayer.(*layers.TCP).DstPort == 1080 || tcpLayer.(*layers.TCP).SrcPort == 1080) {
+			tcp, _ := tcpLayer.(*layers.TCP)
+			ip, _ := packet.Layer(layers.LayerTypeIPv4).(*layers.IPv4)
+			flags := &tcpFlags{FIN: tcp.FIN, ACK: tcp.ACK, RST: tcp.RST,
+				PSH: tcp.PSH, URG: tcp.URG, ECE: tcp.ECE, CWR: tcp.CWR, NS: tcp.NS, SYN: tcp.SYN}
+			stringFlags := parseFlagsToString(flags)
+			if len(stringFlags) != 0 {
+				fmt.Printf("%s:%d -> %s:%d [%s]\n", ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort, stringFlags[0:len(stringFlags)-1])
+			}
+		}
+	}
+}
