580c7688da1ce042478cf25791bd3a795ab941e8
p1nant0m <wgblike@outlook.com>
2022-05-12 22:52:24 +0800

    update: create service command template

---
+++ cli/cmd/service.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+const (
+	shortDescription_service = ""
+	longDescription_service  = ""
+)
+
+// serviceCmd represents the service command
+var serviceCmd = &cobra.Command{
+	Use:   "service",
+	Short: shortDescription_service,
+	Long:  longDescription_service,
+	Run:   serviceCommandRunFunc,
+}
+
+func serviceCommandRunFunc(cmd *cobra.Command, args []string) {
+
+}
+
+func init() {
+	rootCmd.AddCommand(serviceCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serviceCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serviceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
