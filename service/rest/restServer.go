f7a3b26643c1cb65e7f62ec1080f1e5c6bb45bb6
p1nant0m <wgblike@outlook.com>
2022-05-16 20:39:20 +0800

    update: adding comments

--- service/rest/restServer.go
+++ service/rest/restServer.go
@@ -23,18 +23,21 @@ const (
 
 var localIPv4 string
 
+// Make Configuration and Start the RESTFUL API Server
 func RestServe(ctx context.Context) {
+
 	if ENABLE_PRODUCTION {
 		gin.SetMode(gin.ReleaseMode)
 	}
+
 	localIPv4 = localIPObtain()
 	redisService := ctx.Value("redis-service").(*service.RedisService)
-	RedisCommGetHandler := prepareRedisGetHandler(redisService)
+
+	// Registration of Handler that we will use in the router
 	getAllSessionHandler := preparegetAllSessionHandler(redisService)
 	getSessionPackets := preparegetSessionPackets(redisService)
 
 	r := gin.Default()
-	r.GET("test/redis/get/:key", RedisCommGetHandler)
 	r.GET("get/all/session", getAllSessionHandler)
 	r.GET("get/session/:key", getSessionPackets)
 	go r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
@@ -46,8 +49,26 @@ func RestServe(ctx context.Context) {
 	}
 }
 
+// preparegetSessionPackets implement the RESTFUL API /get/session/<Key Struct Serd on base64 encoding>
+// Its response will be like if everything goes well
+// {
+// "packets": [
+// {
+// 		"TTL": 64,
+// 		"TcpFlagS": "ACK",
+// 		"PayloadExist": false,
+// 		"Payload": null,
+// 		"PayloadLen": 60,
+// 		"SrcIP": "192.168.176.128",
+// 		"DstIP": "192.168.176.1",
+// 		"SrcPort": 44292,
+// 		"DstPort": 1080,
+// 		"Timestamp": 1652732483,
+// 		"Direction": "Egress"
+// 	}]}
 func preparegetSessionPackets(redisService *service.RedisService) (fn gin.HandlerFunc) {
 	fn = func(c *gin.Context) {
+		// Setting Redis query timeout
 		ctx, cancel := context.WithDeadline(context.TODO(), time.Now().Add(REDIS_QUERY_TIMEOUT))
 		defer cancel()
 
@@ -66,7 +87,7 @@ func preparegetSessionPackets(redisService *service.RedisService) (fn gin.Handle
 			return
 		}
 
-		var direction string
+		var direction string // This will indicate the Data Flow direction between client and server
 		kk := service.DecodeKey(string(key))
 		if kk.DstIP.To4().String() == localIPv4 {
 			// Ingress
@@ -87,6 +108,7 @@ func preparegetSessionPackets(redisService *service.RedisService) (fn gin.Handle
 		redisService.TaskAssign(task, ResultType, uuID)
 
 		select {
+
 		case notifyMsg := <-notifyCh:
 			if notifyMsg.ErrorMsg == redis.Nil {
 				c.JSON(http.StatusInternalServerError, gin.H{
@@ -95,6 +117,7 @@ func preparegetSessionPackets(redisService *service.RedisService) (fn gin.Handle
 			} else {
 				var value_list []interface{}
 				if notifyMsg.ResultType != "[]redis.Z" {
+					// Not Expected ResultType, inconsistency was detected
 					c.JSON(http.StatusInternalServerError, gin.H{
 						"error": fmt.Sprintf("inconsitency between expeted Type []redis.Z and received Type %v", notifyMsg.ResultType),
 					})
@@ -105,10 +128,12 @@ func preparegetSessionPackets(redisService *service.RedisService) (fn gin.Handle
 					if packet, err := service.DecodeValue(session.Member.(string)); err != nil {
 						c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 						return
-					} else {
+					} else { // Value Struct Deserialization is successful
 						if !packet.PayloadExist {
+							// If payload didn't exists will should keep it nil
 							packet.Payload = nil
 						}
+
 						value_list = append(value_list, struct {
 							*service.Value
 							*service.Key
@@ -119,6 +144,7 @@ func preparegetSessionPackets(redisService *service.RedisService) (fn gin.Handle
 				}
 				c.JSON(http.StatusOK, gin.H{"packets": value_list})
 			}
+
 		case <-ctx.Done():
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": "timeout happens when quering redisDB",
@@ -128,13 +154,33 @@ func preparegetSessionPackets(redisService *service.RedisService) (fn gin.Handle
 	return
 }
 
-type lookup struct {
-	Key *service.Key
-	ID  string
-}
-
+// preparegetALLSessionHandler implement the RESTFUL API /get/all/session
+// Its reponse will be like if everything goes well
+// {
+// "sessions": [
+// {
+// 	"Key": {
+// 		"SrcIP": "192.168.176.128",
+// 		"DstIP": "192.168.176.1",
+// 		"SrcPort": 44292,
+// 		"DstPort": 1080
+// 		},
+// 	"ID": "Pf-BAwEBA0tleQH_ggABBAEFU3JjSVABCgABBURzdElQAQoAAQdTcmNQb3J0AQYAAQdEc3RQb3J0AQYAAAAX_4IBBMCosIABBMCosAEB_q0EAf4EOAA="
+// },
+//
+// {
+// 	"Key": {
+// 		"SrcIP": "192.168.176.1",
+// 		"DstIP": "192.168.176.128",
+// 		"SrcPort": 1080,
+// 		"DstPort": 44292
+// 		},
+// 	"ID": "Pf-BAwEBA0tleQH_ggABBAEFU3JjSVABCgABBURzdElQAQoAAQdTcmNQb3J0AQYAAQdEc3RQb3J0AQYAAAAX_4IBBMCosAEBBMCosIAB_gQ4Af6tBAA="
+// 	}
+// ]}
 func preparegetAllSessionHandler(redisService *service.RedisService) (fn gin.HandlerFunc) {
 	fn = func(c *gin.Context) {
+		// Setting Redis Query timeout
 		ctx, cancel := context.WithDeadline(context.TODO(), time.Now().Add(REDIS_QUERY_TIMEOUT))
 		defer cancel()
 
@@ -142,6 +188,7 @@ func preparegetAllSessionHandler(redisService *service.RedisService) (fn gin.Han
 		redisService.Register(uuID)
 		defer redisService.Destory(uuID)
 		notifyCh, _ := redisService.RetrieveChannel(uuID)
+
 		task := func(rdb *redis.Client) (interface{}, error) {
 			value, err := rdb.SMembers(ctx, "sessions").Result()
 			return value, err
@@ -151,27 +198,33 @@ func preparegetAllSessionHandler(redisService *service.RedisService) (fn gin.Han
 
 		select {
 		case notifyMsg := <-notifyCh:
+
 			if notifyMsg.ErrorMsg == redis.Nil {
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"error": "capturer has not captured any packet yet",
 				})
 			} else {
-				var key_list []*lookup
+				var key_list []interface{}
 				if notifyMsg.ResultType != "[]string" {
 					c.JSON(http.StatusInternalServerError, gin.H{
 						"error": fmt.Sprintf("inconsitency between expeted Type []string and received Type %v", notifyMsg.ResultType),
 					})
 				}
+
 				sessionList := notifyMsg.ExecuteResult.([]string)
 				for _, session := range sessionList {
 					key := service.DecodeKey(session)
-					key_list = append(key_list, &lookup{
+					key_list = append(key_list, struct {
+						Key *service.Key
+						ID  string
+					}{
 						Key: key,
 						ID:  base64.URLEncoding.EncodeToString([]byte(session)),
 					})
 				}
 				c.JSON(http.StatusOK, gin.H{"sessions": key_list})
 			}
+
 		case <-ctx.Done():
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": "timeout happens when quering redisDB",
@@ -181,48 +234,7 @@ func preparegetAllSessionHandler(redisService *service.RedisService) (fn gin.Han
 	return
 }
 
-// template handler for redis querying
-func prepareRedisGetHandler(redisService *service.RedisService) (fn gin.HandlerFunc) {
-	fn = func(c *gin.Context) {
-		ctx, cancel := context.WithDeadline(context.TODO(), time.Now().Add(REDIS_QUERY_TIMEOUT))
-		defer cancel()
-
-		key := c.Param("key")
-		uuID := uuid.New().String()
-		redisService.Register(uuID)
-		defer redisService.Destory(uuID)
-		notifyCh, _ := redisService.RetrieveChannel(uuID)
-		task := func(rdb *redis.Client) (interface{}, error) {
-			value, err := rdb.Get(ctx, key).Result()
-			return value, err
-		}
-		ResultType := "string"
-		redisService.TaskAssign(task, ResultType, uuID)
-
-		select {
-		case notifyMsg := <-notifyCh:
-			if notifyMsg.ErrorMsg == redis.Nil {
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"error": fmt.Sprintf("no related value to the key %v", key),
-				})
-			} else {
-				c.JSON(http.StatusOK, gin.H{
-					"Duration":      notifyMsg.Duration,
-					"ResultType":    notifyMsg.ResultType,
-					"Client":        notifyMsg.Client,
-					"ExecuteResult": notifyMsg.ExecuteResult.(string),
-				})
-			}
-		case <-ctx.Done():
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "timeout happens when quering redisDB",
-			})
-		}
-
-	}
-	return
-}
-
+// localIPObtain used to obtain the IPv4 Address of the local machine
 func localIPObtain() string {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
