be5993b2053776ebb48bde706c9ac441e445a909
p1nant0m <wgblike@outlook.com>
2022-05-24 10:04:51 +0800

    update: (policy controller) errors handle

--- service/strategy/main/client.go
+++ service/strategy/main/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -18,7 +19,13 @@ import (
 	"github.com/sirupsen/logrus"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
+)
+
+const (
+	KEEP_ALIVE_TIMEOUT = 65
 )
 
 type PolicyGenerator interface {
@@ -34,35 +41,50 @@ type remoteHostCache struct {
 }
 
 var (
-	remoteHost remoteHostCache = remoteHostCache{Storage: make(map[string]string)}
-	creds      credentials.TransportCredentials
+	remoteHost remoteHostCache                  = remoteHostCache{Storage: make(map[string]string)} // Take local records for cluster's nodes
+	connCache  map[string]*grpc.ClientConn      = make(map[string]*grpc.ClientConn)                 // Reuse the long-alive connection
+	creds      credentials.TransportCredentials                                                     // Credentials used for TLS connection to the gRPC server
+	slowQueue  chan string                      = make(chan string, 10)
 )
 
 type testPolicyGen struct {
+	mu     sync.Mutex // using for protecting the operation in Policy
 	Policy []string
 }
 
 func (tGen *testPolicyGen) Read() string {
+	tGen.mu.Lock()
+	defer tGen.mu.Unlock()
 	return strings.Join(tGen.Policy, " ")
 }
 
 func (tGen *testPolicyGen) Append(policy string) {
+	tGen.mu.Lock()
+	defer tGen.mu.Unlock()
 	tGen.Policy = append(tGen.Policy, policy)
 }
 
 func (tGen *testPolicyGen) Generate(ctx context.Context) {
 	newRules := "172.17.0.1"
 	tGen.Append(newRules)
+	tGen.mu.Lock()
 	for nodeID, IPAddr := range remoteHost.Storage {
 		go sendInstallStrategyRPC(ctx, IPAddr, nodeID, tGen) //TODO: Implement Failure Dection and retry latter
 	}
+	tGen.mu.Unlock()
 }
 
 func (tGen *testPolicyGen) ToByte() []byte {
+	tGen.mu.Lock()
+	defer tGen.mu.Unlock()
 	return []byte(tGen.Read())
 }
 
+// makeTLSConfiguration return crendentials for TLS Connection based on client key and client's
+// certificate, it will also load CA certificate to verify the server certificate during
+// TLS handshake
 func makeTLSConfiguration() credentials.TransportCredentials {
+	// TODO: Move "credsRootPath" reading from Config file
 	credsRootPath := "../x509/"
 	certificate, err := tls.LoadX509KeyPair(credsRootPath+"client.crt", credsRootPath+"client.key")
 	if err != nil {
@@ -106,22 +128,54 @@ func main() {
 	var testGen PolicyGenerator = &testPolicyGen{}
 	go testGen.Generate(ctx)
 	go nodeWatcher(ctx)
+	go retryConn()
 }
 
-func makeClient(ipAddr string) strategy.StrategyClient {
-	conn, err := grpc.Dial(ipAddr, grpc.WithTransportCredentials(creds))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	conn.GetState() // TODO: Keep-alive for long connection, using timeout to close(), when reuse the connection, check it state before using
-	defer conn.Close()
+func makeClient(ipAddr string) (strategy.StrategyClient, error) {
+	var (
+		conn   *grpc.ClientConn
+		err    error
+		exists bool
+	)
 
+	// Firstly Check whether there is a long-alive connection for the client
+	if conn, exists = connCache[ipAddr]; exists && conn.GetState() == connectivity.Connecting {
+		// Cache Connection can be reused
+		goto out
+	} else {
+		conn, err = grpc.Dial(ipAddr, grpc.WithTransportCredentials(creds),
+			grpc.WithKeepaliveParams(keepalive.ClientParameters{Timeout: KEEP_ALIVE_TIMEOUT}))
+		if err != nil {
+			// Slowly Retry making connection between client and server
+			slowQueue <- ipAddr
+			return nil, fmt.Errorf("did not connect:%v", err.Error())
+		}
+		// Closing the old Connection if it exists
+		if _, exists = connCache[ipAddr]; exists {
+			connCache[ipAddr].Close()
+		}
+		// old connection will be GC
+		connCache[ipAddr] = conn
+	}
+
+out:
 	c := strategy.NewStrategyClient(conn)
-	return c
+	return c, nil
+}
+
+func retryConn() {
+	for ipAddr := range slowQueue {
+		time.Sleep(time.Second * 5)
+		makeClient(ipAddr)
+	}
 }
 
 func sendInstallStrategyRPC(ctx context.Context, ipAddr string, nodeID string, policyGen PolicyGenerator) {
-	c := makeClient(ipAddr)
+	c, err := makeClient(ipAddr)
+	if err != nil {
+		logrus.Warnf("[Policy Controller] error occurs when making Client err=", err.Error())
+		return
+	}
 
 	// Contact the server and print out its response.
 	ctxT, cancel := context.WithTimeout(ctx, time.Second*1)
@@ -152,10 +206,10 @@ func nodeWatcher(ctx context.Context) {
 					// Node Server Instance Disconnnected from clusters
 					delete(remoteHost.Storage, string(event.Kv.Key)) // remove disconnected server from localcache
 					remoteHost.mu.Unlock()
-
 				case clientv3.EventTypePut:
 					// New Node Server Instance Connected to clusters
 					remoteHost.Storage[string(event.Kv.Key)] = string(event.Kv.Value)
+					// TODO: Sending InstallStrategyRPC to new Node
 					remoteHost.mu.Unlock()
 
 				default:
@@ -170,8 +224,4 @@ func nodeWatcher(ctx context.Context) {
 			}
 		}
 	}()
-}
-
-func notify(ctx context.Context) {
-
 }
